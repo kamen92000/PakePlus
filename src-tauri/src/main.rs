47910a1db180c2ec6ef6dadd65d63a54847b985c
kamen92000 <141897455+kamen92000@users.noreply.github.com>
2024-12-04 20:10:17 +0800

    update main rust from pakeplus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::{Menu, MenuItem, Submenu};
-// 对command单独管理
-mod command;
 
 fn main() {
     let edit_menu = Submenu::new(
@@ -19,19 +17,21 @@ fn main() {
             .add_native_item(MenuItem::Quit),
     );
     tauri::Builder::default()
+        .setup(|app| {
+            let _window = tauri::WindowBuilder::new(
+                app,
+                "PakePlus",
+                tauri::WindowUrl::App("http://www.xiaohongshu.com/explore".into()),
+            )
+            .initialization_script(include_str!("./extension/custom.js"))
+            .title("小红书")
+            .inner_size(1024.0, 768.0)
+            .center()
+            .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15")
+            .build()?;
+            Ok(())
+        })
         .menu(Menu::new().add_submenu(edit_menu))
-        .invoke_handler(tauri::generate_handler![
-            command::pakeplus::open_window,
-            command::pakeplus::preview_from_config,
-            command::pakeplus::update_build_file,
-            command::pakeplus::update_config_file,
-            command::pakeplus::update_cargo_file,
-            command::pakeplus::update_main_rust,
-            command::pakeplus::update_custom_js,
-            command::pakeplus::content_to_base64,
-            command::pakeplus::update_config_json,
-            command::pakeplus::rust_main_window,
-        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
